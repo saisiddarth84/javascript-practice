3e6793221ec6c3e3e239264bb307652da1c898e6
saisiddarth84 <145665427+saisiddarth84@users.noreply.github.com>
2023-12-28 17:56:24 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3)  // we are using nodejs, not browser
+
+console.log(3 + 3)
+
+//code readability should be high
+
+console.log("Siddarth")
+
+
+let name = "siddarth"
+let age = 21;
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53 (range)
+// bigint => to store large numbers
+// strint => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
