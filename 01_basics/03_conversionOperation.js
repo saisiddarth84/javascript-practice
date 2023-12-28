d4060a778cf690c1faa72351db98ed241b7f3af4
saisiddarth84 <145665427+saisiddarth84@users.noreply.github.com>
2023-12-28 18:57:09 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "siddarth"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score) 
+//console.log(typeof valueInNumber); // number
+//console.log(valueInNumber); // NaN - not a number
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "siddarth"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber); // string
+let 
