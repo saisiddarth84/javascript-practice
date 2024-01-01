b668104c53ea6511fbe7011cf06293e1b60535f6
saisiddarth84 <145665427+saisiddarth84@users.noreply.github.com>
2024-01-01 13:01:49 +0000

    object methods and de-structuring

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -20,7 +20,8 @@ const regularUser = {
 
 console.log(regularUser.fullname.userfullname.firstname);// "hitesh"
 
-// merge the objects
+
+// merging the objects
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "c", 4: "d"}
@@ -34,7 +35,7 @@ const obj4 = {5: "e", 6: "f"}
 //const obj3 = Object.assign({}, obj1, obj2, obj4) 
 
 
-// spread operator - mostly used to merge the objects
+// spread operator(...) - mostly used to merge the objects
 
 const obj3 = {...obj1, ...obj2}; 
 console.log(obj3); // { '1': 'a', '2': 'b', '3': c, '4': 'd'}
